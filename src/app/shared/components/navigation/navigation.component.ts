d950a4134519701e8d94d65b01e7556d3da11ce6
MihaiOltean2563 <mihaioltean2563@gmail.com>
2018-05-25 10:35:17 +0100

    better icons for nav links

--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -1,61 +1,77 @@
-import { Component, OnInit, ViewChildren, ElementRef, QueryList, AfterViewInit, Input, HostListener, ViewChild, Output, EventEmitter } from '@angular/core';
-import { ComponentItemDirective } from '../../directives/component-item.directive';
+import {
+  Component,
+  OnInit,
+  ViewChildren,
+  ElementRef,
+  QueryList,
+  AfterViewInit,
+  Input,
+  HostListener,
+  ViewChild,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import { ComponentItemDirective } from "../../directives/component-item.directive";
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  selector: "app-navigation",
+  templateUrl: "./navigation.component.html",
+  styleUrls: ["./navigation.component.scss"]
 })
 export class NavigationComponent implements OnInit, AfterViewInit {
-
-  constructor() { }
+  constructor() {}
 
   @Input() activeComponent: number;
   public currentComponent: number = 0;
 
   // @Output() scrollThisElemIntoView: EventEmitter<ElementRef> = new EventEmitter<ElementRef>();
-  @Output() scrollThisElemIntoView: EventEmitter<number> = new EventEmitter<number>();
+  @Output()
+  scrollThisElemIntoView: EventEmitter<number> = new EventEmitter<number>();
 
-  @ViewChildren('listItems', {read: ElementRef}) 
+  @ViewChildren("listItems", { read: ElementRef })
   private listItems: QueryList<ElementRef>;
 
-
   private navListItems = [
-    { icon: 'home', label: 'Home'},
-    { icon: 'info-circle', label: 'About'},
-    { icon: 'list-alt', label: 'Services'},
-    { icon: 'phone', label: 'Contact'},
+    { icon: "home", label: "Home" },
+    { icon: "info-circle", label: "About" },
+    { icon: "list-alt", label: "Services" },
+    { icon: "phone", label: "Contact" }
   ];
-  
-  ngOnInit() {
-  }
-  ngAfterViewInit(){
+
+  ngOnInit() {}
+  ngAfterViewInit() {
     // console.log('listItems: ', this.listItems);
   }
 
-  isActive(i){
-    return (this.activeComponent === i) ? true : false;
+  isActive(i) {
+    return this.activeComponent === i ? true : false;
   }
 
-  @HostListener('window:wheel', ['$event']) onWheelScroll(event: any){
-    if(event.deltaY > 0){ //scrolling down
+  @HostListener("window:wheel", ["$event"])
+  onWheelScroll(event: any) {
+    if (event.deltaY > 0) {
+      //scrolling down
       this.next();
-    }else{  //scrolling up
+    } else {
+      //scrolling up
       this.prev();
     }
   }
 
-  next(){
-    if(this.currentComponent + 1 === this.navListItems.length) return;
-    this.currentComponent = (this.currentComponent + 1 ) % this.navListItems.length;
+  next() {
+    if (this.currentComponent + 1 === this.navListItems.length) return;
+    this.currentComponent =
+      (this.currentComponent + 1) % this.navListItems.length;
   }
-  prev(){
-    if(this.currentComponent === 0) return;
-    this.currentComponent = ((this.currentComponent - 1) + this.navListItems.length) % this.navListItems.length;    
+  prev() {
+    if (this.currentComponent === 0) return;
+    this.currentComponent =
+      (this.currentComponent - 1 + this.navListItems.length) %
+      this.navListItems.length;
   }
-  goToSection(index: number){
+  goToSection(index: number) {
     console.log("Go to section: " + index);
     this.scrollThisElemIntoView.emit(index);
+    // this.isActive(index);
   }
-
 }
